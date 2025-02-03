c6c3eafe1be9c2eedfe0ad68583d57a87e5d20f5
EnviSpec <97458247+EnviSpec@users.noreply.github.com>
2025-02-03 17:22:58 +0530

    Update Stock_Analyzer.py

--- Stock_Analyzer.py
+++ Stock_Analyzer.py
@@ -117,10 +117,10 @@ elif selected_tab == "Fundamental Value of a Stock":
     if len(stock_ticker) > 0:
         symbol = stock_ticker
         #tickers = analyze.company_industry(symbol,analyze.company_information())
-        tickers = analyze.company_industry(symbol,analyze.company_information()) # Testing of Nifty 200
+        tickers = analyze.company_industry(symbol,analyze.company_information_200()) # Testing of Nifty 200
 
         #finance_company = ['HDFCBANK.NS','AXISBANK.NS','ICICIBANK.NS','INDUSINDBK.NS','KOTAKBANK.NS','SBIN.NS','HDFCLIFE.NS', 'BAJFINANCE.NS', 'BAJAJFINSV.NS', 'SBILIFE.NS']
-        nifty_50 = analyze.company_information()
+        nifty_50 = analyze.company_information_200()
         finance_company = nifty_50[nifty_50['Sector'] == 'Financial Services']['Symbol'].to_list()
 
         if len(tickers)>=2:
